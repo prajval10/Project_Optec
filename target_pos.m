dc1ac8dc4a79ab7c7a1bd3de22b431f0870bcc82
Prajval Kumar <prajval10@gmail.com>
2017-03-16 14:47:23 +0100

    Comments added in target_pos

--- target_pos.m
+++ target_pos.m
@@ -4,15 +4,17 @@
 clear all
 clc
 close all
-
+%% 
+%Declare initial and final positions required
 robot_initial_pos=[1, 2];
-robot_final_pos=[5, 5];
+robot_final_pos=[10, 10];
 f=pathplan(robot_initial_pos,robot_final_pos);
 environment;
 pos=[robot_initial_pos;f;robot_final_pos];
-%plot(robot_initial_pos,'x');
-%plot(robot_final_pos,'x');
 
+
+%% 
+%Plotting functions included here
 plot([robot_initial_pos(1),f(1,1)],[robot_initial_pos(2),f(1,2)],'b','LineWidth',2)
 plot([f(2,1),f(1,1)],[f(2,2),f(1,2)],'r','LineWidth',2)
 plot([f(2,1),robot_final_pos(1)],[f(2,2),robot_final_pos(2)],'y','LineWidth',2)
